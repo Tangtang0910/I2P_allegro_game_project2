92d382e398cbdaebd1cc80435ed530b84f065796
Frank Lin <f964966828@gmail.com>
2024-01-07 00:40:48 +0800

    add window.h

---
+++ window/Window.h
@@ -0,0 +1,24 @@
+#ifndef WINDOW_H_INCLUDED
+#define WINDOW_H_INCLUDED
+
+#include "../global.h"
+
+class Window {
+public:
+    Window();
+    ~Window();
+
+    virtual bool process(ALLEGRO_EVENT event);
+    virtual void draw();
+
+    void set_background(string filename);
+    ALLEGRO_BITMAP *get_background();
+
+    bool mouse_click(int startx, int starty, int width, int height, ALLEGRO_EVENT event);
+    void draw_background();
+
+private:
+    ALLEGRO_BITMAP *background = NULL;
+};
+
+#endif
