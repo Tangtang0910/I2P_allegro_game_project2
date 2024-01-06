2a1da73fa9175978cfd9ac19fa2659026a4ca86a
Frank Lin <f964966828@gmail.com>
2024-01-07 00:42:04 +0800

    remove main.cpp

--- Main.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameController.h"
-
-int main(int argc, char *argv[]) {
-    GameController *controller= new GameController();
-    controller->game_establish();
-    delete controller;
-    return 0;
-}
